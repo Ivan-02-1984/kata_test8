c1bd76d7226798753065c5f79d277e2c0fb55a60
Ivan <ivan-163@mail.ru>
2025-02-23 20:21:50 +0400

    Fixed3

--- src/main/java/web/config/EntityManagerConfig.java
+++ src/main/java/web/config/EntityManagerConfig.java
@@ -43,7 +43,7 @@ public class EntityManagerConfig {
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(Objects.requireNonNull(env.getProperty("spring.datasource.driver-class-name")));
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
         dataSource.setUrl(env.getProperty("spring.datasource.url"));
         dataSource.setUsername(env.getProperty("spring.datasource.username"));
         dataSource.setPassword(env.getProperty("spring.datasource.password"));
